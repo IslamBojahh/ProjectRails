73d4eeccd1deccc154db6d6e2cdffca848924d2b
IslamBojah <islambojah@gmail.com>
2019-08-23 01:07:33 +0300

    file report

--- Lecture#2.py
+++ Lecture#2.py
@@ -42,5 +42,49 @@ def URL_parser(url):
     url_dic['netloc']=url[schema+3:netloc]
     url_dic['path']=url[netloc+1:-1]
     return url_dic
+
+
+#q4
+from datetime import datetime
+def event_Report():
+    input_file=open('L2_q5_input.txt')
+    output_file=open('L2_q5_output.txt' ,'w+')
+    lines=input_file.readlines()
+    Days={}
+    FMT = '%H:%M'
+    day=''
+    for i in range(len(lines)):
+        if lines[i][0:4]=='Day ':
+            Days[(lines[i][0:5] , lines[i][6:-1])]=[]
+            day=(lines[i][0:5] , lines[i][6:-1])
+        elif lines[i][0]=="0" or lines[i][0]=="1" or lines[i][0]=="2":
+            if lines[i][6:-1]!='End':
+                Days[day].append([lines[i][:5] , lines[i][6:-1]])
+            else:
+                Days[day].append([lines[i][:5] , lines[i][6:-1]])
     
+    total=0
+    all_activities={}   
+    for day in Days.keys():
+        day_activity=Days[day]
+        output_file.write(day[0]+':'+day[1]+'\n')
+        for i in range(len(day_activity)):
+            if day_activity[i][1]!='End':
+                tdelta = datetime.strptime(day_activity[i+1][0], FMT) - datetime.strptime(day_activity[i][0], FMT)
+                tdelta_min=tdelta.seconds/60
+                total+=tdelta_min
+                if day_activity[i][1] in all_activities:
+                    all_activities[day_activity[i][1]]+=tdelta_min
+                else:
+                    all_activities[day_activity[i][1]]=tdelta_min
+                output_file.write(day_activity[i][0] + '-'+ day_activity[i+1][0] +' ' + day_activity[i][1]+'\n')
+        output_file.write('\n')
+
+    output_file.write('Report\n\n')
+    for activity in sorted(all_activities.keys()):
+        output_file.write(activity +' '+str(int(all_activities[activity]))+' minutes  ' +str(int((all_activities[activity]/total)*100))+'%\n')
+
+    input_file.close()
+    output_file.close()
     
+     
